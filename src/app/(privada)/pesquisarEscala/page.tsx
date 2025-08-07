bce267b0d3104e288cf0211f2b4e0fc6d7079ed4
caadpo <caadpo@gmail.com>
2025-08-07 18:21:29 -0300

    corroções da aplicação

--- src/app/(privada)/pesquisarEscala/page.tsx
+++ src/app/(privada)/pesquisarEscala/page.tsx
@@ -197,7 +197,7 @@ export default function PesquisarEscala() {
 
     try {
       const res = await fetch(
-        `/api/pjesoperacao/by-codop/${encodeURIComponent(codOp)}`
+        `/pjesoperacao/by-codop/${encodeURIComponent(codOp)}`
       );
       const data = await res.json();
 
