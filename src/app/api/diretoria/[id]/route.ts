08bec3fa47763fbefe00d12c82c0cdadb9dd787a
caadpo <caadpo@gmail.com>
2025-10-09 19:18:07 -0300

    feat: Ajuste em varios arquivos

--- src/app/api/diretoria/[id]/route.ts
+++ src/app/api/diretoria/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8081";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://191.252.214.36:4000";
 
 export async function GET(request: NextRequest) {
   const token = request.cookies.get("accessToken")?.value;
